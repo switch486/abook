8357a64d38d04c5d3b2898835e9a5483a51dc534
Adam Puchalski <adam.puchalski@partner.bmwgroup.com>
2024-11-29 15:16:06 +0100

    debugging

--- src/chromecast.py
+++ src/chromecast.py
@@ -23,43 +23,6 @@ def getAvailableChromecasts(currentDialogContext):
             f"  '{device.friendly_name}' ({device.model_name}) @ {device.host}:{device.port} uuid: {device.uuid}"
         )
 
-    return map(lambda o: o.friendly_name, devices)
+    return list(map(lambda o: o.friendly_name, devices))
     
-    
-    
-    
-    
-    
-    
-    
-    #currentDialogContext.zeroconf = zeroconf.Zeroconf()
-    #browser = pychromecast.CastBrowser(
-    #    pychromecast.SimpleCastListener(
-    #        lambda uuid, 
-    #        service: print(browser.devices[uuid].friendly_name)), 
-    #    currentDialogContext.zeroconf)
-    #browser.start_discovery()
-    ##TODO - somehow gather the serach results
-    #pychromecast.discovery.stop_discovery(browser)
-    #return 'some List'
-
-
-#TODO - test it!
-
-#https://github.com/home-assistant-libs/pychromecast/blob/master/examples/discovery_example2.py
-#devices, browser = pychromecast.discovery.discover_chromecasts(
-#    known_hosts=args.known_host
-#)
-## Shut down discovery
-#browser.stop_discovery()
-#
-#print(f"Discovered {len(devices)} device(s):")
-#for device in devices:
-#    print(
-#        f"  '{device.friendly_name}' ({device.model_name}) @ {device.host}:{device.port} uuid: {device.uuid}"
-#    )
-#    if args.verbose:
-#        print(f"  service: {device}")
-
-
 # TODO - sort list alphabetically so that the lastly used is on top
