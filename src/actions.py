03d3ccf0f5c1fa89f9e9b85049cbe257ca41a98f
Adam Puchalski <adam.puchalski@partner.bmwgroup.com>
2024-11-29 15:32:49 +0100

    debugging

--- src/actions.py
+++ src/actions.py
@@ -5,7 +5,7 @@ import chromecast
 def LOAD_CAST_DEVICES(currentDialogContext) : 
     currentDialogContext.lastCastDevice = filesystem.loadSystemProperty(SYSTEM_PROPERTIES.LAST_CAST_DEVICE)
     currentDialogContext.chromecastDevices = chromecast.getAvailableChromecasts(currentDialogContext)
-    if currentDialogContext.chromecastDevices in currentDialogContext.chromecastDevices:
+    if currentDialogContext.lastCastDevice in currentDialogContext.chromecastDevices:
         currentDialogContext.menu_chooseCast_CursorLocationAbsolute = currentDialogContext.chromecastDevices.index(currentDialogContext.lastCastDevice)
     return currentDialogContext
 
