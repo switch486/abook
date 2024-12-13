ee1018e28376a68ee44ffe03a7f7fa72be3f6277
Adam Puchalski <adam.puchalski@partner.bmwgroup.com>
2024-12-13 22:14:17 +0100

    ip fixes

--- src/actions.py
+++ src/actions.py
@@ -44,8 +44,11 @@ def PLAY_AUDIOBOOK(currentDialogContext):
 
 
 def getIPOfCurrentMachine():
-    hostname = socket.gethostname()
-    return socket.gethostbyname(hostname)
+    local_hostname = socket.gethostname()
+    ip_addresses = socket.gethostbyname_ex(local_hostname)[2]
+    filtered_ips = [ip for ip in ip_addresses if not ip.startswith("127.")]
+    first_ip = filtered_ips[:1]
+    return first_ip
 
 
 def playPassedAudiobook(currentDialogContext, currentBook):
