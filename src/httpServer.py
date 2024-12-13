8b2ccaffb99050563dfccecb5ca90ecc317e2dce
Adam Puchalski <adam.puchalski@partner.bmwgroup.com>
2024-12-13 19:29:57 +0100

    fix

--- src/httpServer.py
+++ src/httpServer.py
@@ -3,8 +3,9 @@ import functools
 
 
 def launchHttpServerInDirectory(port, path):
+    print('Launch HTTP Server for localhost and port:' + port)
     Handler = functools.partial(SimpleHTTPRequestHandler, directory=path)
-    httpd = HTTPServer(('localhost', port), Handler)
+    httpd = HTTPServer(('localhost', int(port)), Handler)
     httpd.serve_forever()
     return httpd
 
